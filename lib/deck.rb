0c82060da2f3c83ecebbc69fc6a6c440ef4a2133
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-15 15:37:47 -0400

    Update deck & deck spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,9 @@
+require_relative '../lib/card' 
+
 class Deck
     attr_reader :cards
 
-    def initialize(cards)
+    def initialize(cards = [])
         @cards = cards
         # @rankings = @cards.map { |card| card.rank }
         # .map takes each element of og array & passes it to block {}, transforms, & creates new array
@@ -13,6 +15,16 @@ class Deck
 
     def high_ranking_cards
         @cards.select { |card| card.rank >= 11 }
+        #filters array |element| based on condition & returns new array
     end
 
+    def percent_high_ranking
+        (high_ranking_cards.length.to_f / @cards.length * 100).round(2)
+    end
 end
+
+cards = [card1 = Card.new(:diamond, 'Queen', 12), card2 = Card.new(:spade, '3', 3), card3 = Card.new(:heart, 'Ace', 14)]
+
+deck = Deck.new(cards)
+
+# require 'pry'; binding.pry
